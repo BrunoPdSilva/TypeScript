319057ebe1bb7b0e1f488baa0374aed66e01a478
Bruno Peres <bruno_16515@hotmail.com>
2022-07-15 11:25:24 -0300

    Lesson-02 concluída

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = "Luigi";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
