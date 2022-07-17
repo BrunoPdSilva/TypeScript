38ad93e9fd21a18edb8de4745d29c95c52982964
Bruno Peres <bruno_16515@hotmail.com>
2022-07-17 11:24:23 -0300

    Lesson-17 finalizada

--- public/app.js
+++ public/app.js
@@ -1,34 +1,15 @@
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/Payment.js";
-/* let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice("Yoshi", "web work", 250);
-docTwo = new Payment("Mario", "plumbing work", 200);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
-console.log(docs); */
-/* const invOne = new Invoice("Bruno", "Livros", 200);
-const invTwo = new Invoice("Selma", "Shopee", 270);
-
-//Array que só recebe dados do tipo(class) Invoice
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach(inv => {
-  console.log(inv.client, inv.amount, inv.format());
-});
- */
+import { ListTemplate } from "./classes/ListTemplate.js";
 const form = document.querySelector(".new-item-form");
 //inputs
 const type = document.querySelector("#type");
 const tofrom = document.querySelector("#tofrom");
 const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+const list = new ListTemplate(ul);
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     let doc;
@@ -38,5 +19,5 @@ form.addEventListener("submit", (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, "end");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,7 @@
 import { Invoice } from "./classes/invoice.js";
 import { Payment } from "./classes/Payment.js";
 import { HasFormatter } from "./interfaces/HasFormatter.js";
-
-/* let docOne: HasFormatter;
-let docTwo: HasFormatter;
-
-docOne = new Invoice("Yoshi", "web work", 250);
-docTwo = new Payment("Mario", "plumbing work", 200);
-
-let docs: HasFormatter[] = [];
-docs.push(docOne);
-docs.push(docTwo);
-
-console.log(docs); */
-
-/* const invOne = new Invoice("Bruno", "Livros", 200);
-const invTwo = new Invoice("Selma", "Shopee", 270);
-
-//Array que só recebe dados do tipo(class) Invoice
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach(inv => {
-  console.log(inv.client, inv.amount, inv.format());
-});
- */
+import { ListTemplate } from "./classes/ListTemplate.js";
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -34,6 +10,10 @@ const type = document.querySelector("#type") as HTMLSelectElement;
 const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
 
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
@@ -46,5 +26,5 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, "end");
 });
